34f563d03fc6d80110b7be07ac75b9d00a4e5b16
cetinsamet <cetin.samet@metu.edu.tr>
2023-10-17 21:49:40 +0200

    remove bash file

--- src/run.sh
+++
@@ -1,6 +0,0 @@
-python predict_et.py \
-    --pretrained_model ../models/et-estimator.pt \
-    --s1_tensor ../data/tensors/s1/s1_0006.pt \
-    --era5_tensor ../data/tensors/era5/era5_0006.pt \
-    --dem_tensor ../data/tensors/dem/dem_0006.pt \
-    --device cuda
